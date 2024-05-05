3b3c79cfc7247bd5f81096f6191b63655c2dd19b
Tarasov Oleg <tarasovom@mail.ru>
2024-05-05 13:45:04 +0300

    123

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -20,48 +20,69 @@ public class Main {
 
         if (src.mkdir()) {
             StringBuilder.append("Каталог src создан \n");
+        } else {
+            StringBuilder.append("Каталог src не создан \n");
         }
 
         if (res.mkdir()) {
             StringBuilder.append("Каталог res создан \n");
+        } else {
+            StringBuilder.append("Каталог res не создан \n");
         }
 
         if (saveGames.mkdir()) {
             StringBuilder.append("Каталог saveGames создан \n");
+        } else {
+            StringBuilder.append("Каталог saveGames не создан \n");
         }
 
         if (temp.mkdir()) {
             StringBuilder.append("Каталог temp создан \n");
+        } else {
+            StringBuilder.append("Каталог temp не создан \n");
         }
 
         if (main.mkdir()) {
             StringBuilder.append("Каталог main создан \n");
+        } else {
+            StringBuilder.append("Каталог main не создан \n");
         }
 
         if (test.mkdir()) {
             StringBuilder.append("Каталог test создан \n");
+        } else {
+            StringBuilder.append("Каталог test не создан \n");
         }
 
         File mainJava = new File("E://Games/src/main/main.java");
         File utils = new File("E://Games/src/main/utils.java");
         try {
             if (mainJava.createNewFile()) StringBuilder.append("Файл mainJava был создан \n");
+            else StringBuilder.append("Файл mainJava не был создан \n");
             if (utils.createNewFile()) StringBuilder.append("Файл utils был создан \n");
+            else StringBuilder.append("Файл utils не был создан \n");
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
 
         File drawables = new File("E://Games/res/drawables");
-        if (drawables.mkdir())
+        if (drawables.mkdir()) {
             StringBuilder.append("Каталог drawables создан \n");
+        }else {
+            StringBuilder.append("Каталог drawables не создан \n");
+        }
 
         File vectors = new File("E://Games/res/vectors");
-        if (vectors.mkdir())
+        if (vectors.mkdir()) {
             StringBuilder.append("Каталог vectors создан \n");
-
+        } else {
+            StringBuilder.append("Каталог vectors создан \n");
+        }
         File icons = new File("E://Games/res/icons");
         if (icons.mkdir()) {
             StringBuilder.append("Каталог icons создан \n");
+        } else{
+            StringBuilder.append("Каталог icons не создан \n"); 
         }
 
         File tempTxt = new File("E://Games/temp/temp.txt");
